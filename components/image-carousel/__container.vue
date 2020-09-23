4d92c3a6879e6d96045acdae894bb0a5a4193917
lorrod <mulin.pasha@gmail.com>
2020-09-23 18:00:23 +0300

    scared animation

--- components/image-carousel/__container.vue
+++ components/image-carousel/__container.vue
@@ -6,7 +6,7 @@
       :key="key">
         <imageDiv
           :image-name="name"
-          :style="{ 'margin-left': '-'+(100 * currentImageIndex)+'%' }"
+          :id="'image-'+name"
         />
       </div>
       <paging
@@ -15,14 +15,14 @@
         :currentDot="this.$store.state.shownImageIndexPrimary"
         :count-pages="this.$store.state.imageArray.length" />
     </div>
+    <button @click="checking" class="checkingButton">Check button</button>
   </div>
 </template>
 
 <script>
 import imageDiv from "./__image"
 import paging from "./__paging"
-
-import { mapMutations } from 'vuex'
+import { TimelineLite } from 'gsap'
 export default {
 name: "container",
   components: {
@@ -43,22 +43,55 @@ name: "container",
       }
     }
   },
+  watch: {
+    currentImageIndex: function (newIndex, oldIndex) {
+      this.hideImage(oldIndex)
+      this.showImage(newIndex)
+    }
+  },
+  methods: {
+    showImage(imgNumber) {
+      const timeline = new TimelineLite()
+      console.log('animate plz')
+      timeline.to("#image-"+this.$store.state.imageArray[imgNumber], .1, { x: '-100%' })
+    },
+    hideImage(imgNumber) {
+      const timeline = new TimelineLite()
+      console.log('animate plz')
+      timeline.to("#image-"+this.$store.state.imageArray[imgNumber], .1, { x: '-200%' })
+    },
+    checking() {
+      console.log('animate plz 1')
+      this.showImage(0)
+    }
+  },
+  created() {
+  }
 }
 </script>
 
 <style lang="scss">
+  .checkingButton {
+    position: fixed;
+    bottom: 40px;
+    left: 40px;
+  }
   .carousel-container {
     width: 100%;
     height: 100%;
     position: relative;
     &__images {
+      /*
       display: flex;
       margin: 0 auto;
+       */
       width: 100%;
       height: 100%;
       overflow: hidden;
     }
     &__image {
+      position:absolute;
+      right:-100%;
       width: 100%;
       height: 100%;
     }
